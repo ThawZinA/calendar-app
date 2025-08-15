dd73398675cc6e50bf2036a6a6f460e34d1dd22c
thawzinag <37143924+ThawZinA@users.noreply.github.com>
2025-08-15 19:10:48 +0100

    Update PublicNavBar.tsx

--- app/components/PublicNavBar.tsx
+++ app/components/PublicNavBar.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
 
 export default function PublicNavBar() {
     
     return (
-        <nav className="flex justify-between items-center fixed z-50 w-full h-28 bg-gray px-10 gap-4 shadow-2xl"></nav>
+        <nav className="flex justify-between items-center fixed z-50 w-full h-28 bg-gray px-10 gap-4 shadow-2xl">
+            <Link 
+                href='/login'
+                className="flex items-center hover:scale-150 duration-500"
+            >
+                <Image
+                    src="/assets/logo.svg"
+                    alt="calendar logo"
+                    width={60}
+                    height={60}
+                />
+            </Link>
+        </nav>
     )
 }
