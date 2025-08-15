809dcc808b30a2b6069ed24f3477490196b5630e
thawzinag <37143924+ThawZinA@users.noreply.github.com>
2025-08-15 12:17:52 +0100

    Added book page

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,17 @@
+import { currentUser } from "@clerk/nextjs/server"
+import PublicNavBar from "../components/PublicNavBar";
+
+export default async function MainLayout({children}: {
+    children:React.ReactNode
+}) {
+    const user = await currentUser();
+    return(
+        <main className="relative">
+            <PublicNavBar />
+            {/* { user ? <PrivateNavBar /> : <PublicNavBar />} */}
+            <section className="pt-36">
+                {children}
+            </section>
+        </main>
+    )
+}
